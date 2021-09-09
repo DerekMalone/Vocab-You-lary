da5ba0c3a6c872c27907ac9fe2707f84b1bb0269
DerekMalone <laxmalone@gmail.com>
2021-09-08 21:10:20 -0500

    set foundation of starting the app, pick up on navbar

--- src/scripts/helpers/startApp.js
+++ src/scripts/helpers/startApp.js
@@ -1,4 +1,7 @@
+import domEvents from '../components/events/domEvents';
 import navigationEvents from '../components/events/navigationEvents';
+import domBuilder from '../components/forms/domBuilder';
+import navBar from '../components/forms/navBar';
 import logoutButton from './logoutButton';
 
 const startApp = () => {
